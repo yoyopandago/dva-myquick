a94499672863fab7c117e39e14869a20cf7e7e7a
yoyopandago <32345958@qq.com>
2018-08-16 10:55:47 +0800

    test back

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ function RouterConfig({ history }) {
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
-        <Route path="/products" exact component={Products} /> //产品
+        <Route path="/products" exact component={Products} />
       </Switch>
     </Router>
   );
